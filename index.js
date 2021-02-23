96b59cac7499388b56af50dbc7c1deb3af7488e9
+ <mcrea23@aol.com>
2021-02-23 15:21:11 -0600

    commit tree c2b93577ecc1dbd8c4f107a7c5e987f58dd521a7

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
-function fetchBooks() {
 
+function fetchBooks() {
+  return fetch('https://anapioficeandfire.com/api/books')
+  .then(resp => resp.json())
+  .then(json => renderBooks(json))
 }
 
-function renderBooks(books) {
+function renderBooks(json) {
   const main = document.querySelector('main')
-  books.forEach(book => {
+  json.forEach(book => {
     const h2 = document.createElement('h2')
-    h2.innerHTML = book.name
+    h2.innerHTML = `<h2>${book.name}</h2>`
     main.appendChild(h2)
   })
 }
